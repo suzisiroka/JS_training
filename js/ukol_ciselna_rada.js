9f66890d4e6a6a72f119bb0e04f7d967c93cb03a
Zuzana Široká <zuzana.sir@seznam.cz>
2023-08-31 17:21:34 +0200

    refactor: remove commented code

--- js/ukol_ciselna_rada.js
+++ js/ukol_ciselna_rada.js
@@ -3,9 +3,6 @@ let y = 0
 
 
 while(i < 21) {
-    // document.write(i)
-    // document.write('<br>') //br znaší break - zalomení na další řádek, tedy to čísla nevypíše za sebe, ale pod sebe
-    
     y++
     if (y===8){
         y=0
